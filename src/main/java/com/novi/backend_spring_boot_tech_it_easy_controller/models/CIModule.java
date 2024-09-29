9f0f53d8ce555cefa566c183acb5f5edb10f9b66
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2024-09-29 13:06:24 +0200

    add variables to all new models

--- src/main/java/com/novi/backend_spring_boot_tech_it_easy_controller/models/CIModule.java
+++ src/main/java/com/novi/backend_spring_boot_tech_it_easy_controller/models/CIModule.java
@@ -18,17 +18,6 @@ public class CIModule {
     private String name;
     @Column(nullable = false)
     private Double price;
-
-    private Double availableSize;
-    private Integer refreshRate;
-    private String screenType;
-    private String screenQuality;
-    private Boolean smartTv;
-    private Boolean wifi;
-    private Boolean voiceControl;
-    private Boolean hdr;
-    private Boolean bluetooth;
-    private Boolean ambiLight;
     private Integer originalStock;
     private Integer sold;
 
@@ -40,23 +29,13 @@ public class CIModule {
     }
 
 
-    public CIModule(Long id, String type, String brand, String name, Double price, Double availableSize, Integer refreshRate, String screenType, String screenQuality, Boolean smartTv, Boolean wifi, Boolean voiceControl, Boolean hdr, Boolean bluetooth, Boolean ambiLight, Integer originalStock, Integer sold){
+    public CIModule(Long id, String type, String brand, String name, Double price, Integer originalStock, Integer sold){
 
         this.id = id;
         this.type = type;
         this.brand = brand;
         this.name = name;
         this.price = price;
-        this.availableSize = availableSize;
-        this.refreshRate = refreshRate;
-        this.screenType = screenType;
-        this.screenQuality = screenQuality;
-        this.smartTv = smartTv;
-        this.wifi = wifi;
-        this.voiceControl = voiceControl;
-        this.hdr = hdr;
-        this.bluetooth = bluetooth;
-        this.ambiLight = ambiLight;
         this.originalStock = originalStock;
         this.sold = sold;
     }
@@ -101,86 +80,6 @@ public class CIModule {
 
     public void setPrice(Double price) {
         this.price = price;
-    }
-
-    public Double getAvailableSize() {
-        return availableSize;
-    }
-
-    public void setAvailableSize(Double availableSize) {
-        this.availableSize = availableSize;
-    }
-
-    public Integer getRefreshRate() {
-        return refreshRate;
-    }
-
-    public void setRefreshRate(Integer refreshRate) {
-        this.refreshRate = this.refreshRate;
-    }
-
-    public String getScreenType() {
-        return screenType;
-    }
-
-    public void setScreenType(String screenType) {
-        this.screenType = screenType;
-    }
-
-    public String getScreenQuality() {
-        return screenQuality;
-    }
-
-    public void setScreenQuality(String screenQuality) {
-        this.screenQuality = screenQuality;
-    }
-
-    public Boolean getSmartTv() {
-        return smartTv;
-    }
-
-    public void setSmartTv(Boolean smartTv) {
-        this.smartTv = smartTv;
-    }
-
-    public Boolean getWifi() {
-        return wifi;
-    }
-
-    public void setWifi(Boolean wifi) {
-        this.wifi = wifi;
-    }
-
-    public Boolean getVoiceControl() {
-        return voiceControl;
-    }
-
-    public void setVoiceControl(Boolean voiceControl) {
-        this.voiceControl = voiceControl;
-    }
-
-    public Boolean getHdr() {
-        return hdr;
-    }
-
-    public void setHdr(Boolean hdr) {
-        this.hdr = hdr;
-    }
-
-    public Boolean getBluetooth() {
-        return bluetooth;
-    }
-
-    public void setBluetooth(Boolean bluetooth) {
-        this.bluetooth = bluetooth;
-    }
-
-    public Boolean getAmbiLight() {
-        return ambiLight;
-    }
-
-    public void setAmbiLight(Boolean ambiLight) {
-        this.ambiLight = ambiLight;
     }
 
     public Integer getOriginalStock() {
