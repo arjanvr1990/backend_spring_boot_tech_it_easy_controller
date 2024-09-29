4129abae501859b5b29c008b8aaadef137e81a85
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2024-09-29 14:26:38 +0200

    Repositories en Dtos toegevoegd

---
+++ src/main/java/com/novi/backend_spring_boot_tech_it_easy_controller/Dtos/CIModuleInputDto.java
@@ -0,0 +1,64 @@
+package com.novi.backend_spring_boot_tech_it_easy_controller.Dtos;
+
+
+
+public class CIModuleInputDto {
+
+    private Long id;
+    private String name;
+    private String brand;
+    private Double price;
+    private Integer originalStock;
+    private Integer sold;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public Integer getSold() {
+        return sold;
+    }
+
+    public void setSold(Integer sold) {
+        this.sold = sold;
+    }
+}
---
+++ src/main/java/com/novi/backend_spring_boot_tech_it_easy_controller/Dtos/WallBracketInputDto.java
@@ -0,0 +1,78 @@
+package com.novi.backend_spring_boot_tech_it_easy_controller.Dtos;
+
+
+public class WallBracketInputDto {
+
+    private Long id;
+    private String name;
+    private String brand;
+    private Double price;
+    private Double availableSize;
+    private Integer originalStock;
+    private Integer sold;
+    private String compatibleWith;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getAvailableSize() {
+        return availableSize;
+    }
+
+    public void setAvailableSize(Double availableSize) {
+        this.availableSize = availableSize;
+    }
+
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public Integer getSold() {
+        return sold;
+    }
+
+    public void setSold(Integer sold) {
+        this.sold = sold;
+    }
+
+    public String getCompatibleWith() {return compatibleWith; }
+
+    public void setCompatibleWith(String compatibleWith) {this.compatibleWith = compatibleWith;}
+
+}
